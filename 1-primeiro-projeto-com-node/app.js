ce9cc5755a6a5379a3882d5bd7d7644f0b964a19
NalbertC <nalberthcastro1510@gmail.com>
2022-06-03 09:05:22 -0300

    Simular requisição GET com insimnia

--- 1-primeiro-projeto-com-node/app.js
+++ 1-primeiro-projeto-com-node/app.js
@@ -3,10 +3,37 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Olá mundo!");
+  res.send("Nalberth");
+});
+
+/*app.get("/contatos", (req, res) => {
+  return res.json({
+    "Nome:" : "Nalberth",
+    "e-mail": "nalberthcastro1510@gmail.com"
+  });
+});*/
+
+// app.get("/contatos/:id", (req, res) => {
+//   const id = req.params.id;
+//   return res.json({
+//     id : id,
+//     Nome: "Nalberth",
+//     email: "nalberthcastro1510@gmail.com"
+//   });
+// });
+
+app.get("/contatos/:id", (req, res) => {
+  const {id} = req.params; //Parâmetro
+  const {sit} = req.query;  //Variável
+  return res.json({
+    id, 
+    Nome: "Nalberth", 
+    email: "nalberthcastro1510@gmail.com",
+    sit
+  });
 });
 
 app.listen(3000, () => {
-    console.log("Servidor iniciado com sucesso na porta 3000: ");
+    console.log("Servidor iniciado com sucesso na porta 3000: http://localhost:3000");
 } );
 
